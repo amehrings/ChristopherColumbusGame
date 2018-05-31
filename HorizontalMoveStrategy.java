a992acfe0a8c03e53026399ccd57217131126d07
Aaron Mehrings <mehringsaaron@gmail.com>
2018-05-30 20:46:50 -0500

    added horizontal class

---
+++ HorizontalMoveStrategy.java
@@ -0,0 +1,16 @@
+
+public class HorizontalMoveStrategy implements Strategy {
+
+	@Override
+	public void movePirateShip() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getStrategy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
