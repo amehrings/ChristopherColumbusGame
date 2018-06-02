c4ad37e106bff60267bd85093eddffc4bbbd5b99
Austin Paul <safeat3rd@hotmail.com>
2018-06-01 23:41:32 -0500

    Updated Container Class

--- Container.java
+++ Container.java
@@ -2,9 +2,54 @@
  * Final Project
  * SE350
  */
- 
- public interface Container {
-	 public void move();
+
+import java.util.ArrayList;
+
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+
+public class Container implements Component{
+	Rectangle rect = new Rectangle();
+	int height;
+	int width;
+	Point2D topLeft;
+	ArrayList<Component> components = new ArrayList<Component>();
+	
+	//Container constructor
+	public Container(Point2D topLeft, int w, int h) {
+		this.topLeft = topLeft;
+		height = h;
+		width = w;
+		rect.setHeight(height);
+		rect.setWidth(width);
+		rect.setX(topLeft.getX());
+		rect.setY(topLeft.getY());
+		rect.setStroke(Color.TRANSPARENT);
+		rect.setFill(Color.TRANSPARENT);
+	}
+	 
+	////returns true if Container object contains the point given
+	public boolean ContainsPoint(Point2D point) {
+		return rect.contains(point);
+	}
+	 
+	 //adds child to component array
+	public void addChild(Component comp) {
+		components.add(comp);
+	}
+	
+	//removes child from component array
+	public void removeChild(Component comp) {
+		components.remove(comp);
+	}
+	
+	@Override
+	public void move() {
+		//empty for the containing class
+		
+	}
 	 
  
  }
