06d188ebba5ceea71f24d5dbcfad97cc0ac6954b
amehrings <mehringsaaron@gmail.com>
2018-06-02 15:34:01 -0500

    ccTest comments

--- Tests/CCTest.java
+++ Tests/CCTest.java
@@ -4,25 +4,26 @@
  */
 
 import static org.junit.Assert.*;
-
 import java.awt.Point;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Collections;
 import org.junit.Test;
 
+//Christopher Columbus Ship tests for location, moving 1 coordinate in every direction, and adding/removing observers to list
 public class CCTest{
 	
 	List<Observer> observers = new LinkedList<Observer>();
 	OceanMap oceanMap = OceanMap.getInstance();
 	
 	@Test
+	//tests if Columbus location is the same as the oceanMap Columbus Location, asserts True
 	public void testGetColumbusLocation(){
 		ChristopherColumbus cc = new ChristopherColumbus(oceanMap);
 		assertTrue(cc.getChristopherColumbusLocation().equals(oceanMap.ChristopherColumbusLocation));
 	}
 	
 	@Test
+	//creates testPoint given old coordinates and tests where it is expected to go + 1 X coordinate, asserts True
 	public void testGoEast(){
 		ChristopherColumbus cc = new ChristopherColumbus(oceanMap);
 		Point testPoint = new Point((int) oceanMap.getChristopherColumbusLocation().getX(), (int) oceanMap.getChristopherColumbusLocation().getY());
@@ -31,6 +32,7 @@ public class CCTest{
 	}
 	
 	@Test
+	//creates testPoint given old coordinates and tests where it is expected to go -1 X coordinate, asserts True
 	public void testGoWest(){
 		ChristopherColumbus cc = new ChristopherColumbus(oceanMap);
 		Point testPoint = new Point((int) oceanMap.getChristopherColumbusLocation().getX(), (int) oceanMap.getChristopherColumbusLocation().getY());
@@ -39,6 +41,7 @@ public class CCTest{
 	}
 
 	@Test
+	//creates testPoint given old coordinates and tests where it is expected to go -1 Y coordinate, asserts True
 	public void testGoNorth(){
 		ChristopherColumbus cc = new ChristopherColumbus(oceanMap);
 		Point testPoint = new Point((int) oceanMap.getChristopherColumbusLocation().getX(), (int) oceanMap.getChristopherColumbusLocation().getY());
@@ -47,6 +50,7 @@ public class CCTest{
 	}
 	
 	@Test
+	//creates testPoint given old coordinates and tests where it is expected to go + 1 Y coordinate, asserts True
 	public void testGoSouth(){
 		ChristopherColumbus cc = new ChristopherColumbus(oceanMap);
 		Point testPoint = new Point((int) oceanMap.getChristopherColumbusLocation().getX(), (int) oceanMap.getChristopherColumbusLocation().getY());
@@ -55,6 +59,7 @@ public class CCTest{
 	}
 	
 	@Test
+	//tests if observer is added to observer list, asserts True
 	public void testAddObserver() {
 		Dutchman d = new Dutchman(oceanMap, "Slow");
 		observers.add(d);
@@ -62,6 +67,7 @@ public class CCTest{
 	}
 
 	@Test
+	//tests if observer is removed from observer list, asserts True
 	public void testRemoveObserver() {
 		Dutchman d = new Dutchman(oceanMap, "Slow");
 		observers.add(d);		
