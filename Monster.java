806ca31e1fa778cac47f2b88419998a7c4aa827f
amehrings <mehringsaaron@gmail.com>
2018-06-02 00:34:58 -0500

    kraken

--- Monster.java
+++ Monster.java
@@ -8,11 +8,10 @@ import javafx.geometry.Point2D;
 public class Monster implements Component {
 	 
 	Monster monster;
+	Point2D currentLocation;
+	boolean direction = true;
 		
-	public Monster(Point2D point) {
-		monster = new Monster(point);
-		monster.setX(point.getX());
-		monster.setY(point.getY());
+	public Monster() {
 	}
 	
 	public Monster getMonster() {
@@ -27,16 +26,15 @@ public class Monster implements Component {
 		return (monster.containsPoint(point));
 	}
 	
-	public void setX(double d){
-		
-	}
-	
-	public void setY(double d){
-		
+	public void place(Point2D point){
+		currentLocation = point; 
 	}
 	
 	public void move(){
-		 
+		Point2D tempPoint = new Point2D(currentLocation.getX() + 1, currentLocation.getY());
+		if (getContainer().ContainsPoint(tempPoint)) {
+			currentLocation = new Point2D(currentLocation.getX() +1, currentLocation.getY());
+		}
 	}
 	 
  
